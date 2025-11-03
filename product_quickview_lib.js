1704143d46272a140df346e9cf1584064b008675
vsanm01 <155801539+vsanm01@users.noreply.github.com>
2025-11-03 16:04:25 +0530

    Update product_quickview_lib.js

--- product_quickview_lib.js
+++ product_quickview_lib.js
@@ -259,7 +259,7 @@
                     }
                     
                     .pqv-btn-primary {
-                        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+                        background: linear-gradient(135deg, #667eea 0%, #ffd814  100%);
                         color: white;
                         flex: 1;
                     }
